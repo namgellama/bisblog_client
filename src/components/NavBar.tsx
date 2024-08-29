d005b7e67b2adb4ffe2a55ae90dd8421c2dc888e
Namgel <namgeltamang14@gmail.com>
2024-08-29 15:45:01 +0545

    Build auth modal

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,24 @@
-import { Box, Button, Flex, Image } from "@chakra-ui/react";
+import { Box, Button, Flex, Image, useDisclosure } from "@chakra-ui/react";
 import logo from "../assets/logo.png";
-import { SearchBar } from "./";
+import { AuthModal, SearchBar } from "./";
 
 const NavBar = () => {
+	const { isOpen, onOpen, onClose } = useDisclosure();
+
 	return (
 		<Box as="nav" py={2} px={5} borderBottom="1px solid #DBE4E9">
 			<Flex justify="space-between">
 				<Image src={logo} alt="Logo" w={150} />
 				<SearchBar />
-				<Button borderRadius={20} colorScheme="purple" px={4}>
+				<Button
+					borderRadius={20}
+					colorScheme="purple"
+					px={4}
+					onClick={onOpen}
+				>
 					Log in
 				</Button>
+				<AuthModal isOpen={isOpen} onClose={onClose} />
 			</Flex>
 		</Box>
 	);
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
+export { default as AuthModal } from "./AuthModal";
+export { default as Layout } from "./Layout";
 export { default as NavBar } from "./NavBar";
 export { default as SearchBar } from "./SearchBar";
