98309986f924037fb550583cb4655132565f8138
Namgel <namgeltamang14@gmail.com>
2024-08-30 02:23:20 +0545

    Set up react-hook-form with zod

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -5,13 +5,38 @@ import {
 	FormControl,
 	FormLabel,
 	Input,
+	Spinner,
 	Text,
 } from "@chakra-ui/react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import { z } from "zod";
+
+const schema = z.object({
+	emailOrUsername: z.string().min(1, "Email or username is required."),
+	password: z.string().min(1, "Password is required."),
+});
+
+type FormFields = z.infer<typeof schema>;
 
 const AuthForm = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isSubmitting },
+		setError,
+	} = useForm<FormFields>({
+		resolver: zodResolver(schema),
+	});
+
+	const onSubmit: SubmitHandler<FormFields> = async (data) => {
+		try {
+		} catch (error) {}
+	};
+
 	return (
-		<Box as="form">
+		<Box as="form" onSubmit={handleSubmit(onSubmit)}>
 			<Flex direction="column" gap={5}>
 				<FormControl>
 					<FormLabel>
@@ -20,7 +45,17 @@ const AuthForm = () => {
 							*
 						</Text>
 					</FormLabel>
-					<Input type="text" border="none" bg="#DBE4E9" />
+					<Input
+						type="text"
+						border="none"
+						bg="#DBE4E9"
+						{...register("emailOrUsername")}
+					/>
+					{errors.emailOrUsername && (
+						<Text color="red">
+							{errors.emailOrUsername.message}
+						</Text>
+					)}
 				</FormControl>
 
 				<FormControl>
@@ -30,7 +65,15 @@ const AuthForm = () => {
 							*
 						</Text>
 					</FormLabel>
-					<Input type="password" border="none" bg="#DBE4E9" />
+					<Input
+						type="password"
+						border="none"
+						bg="#DBE4E9"
+						{...register("password")}
+					/>
+					{errors.password && (
+						<Text color="red">{errors.password.message}</Text>
+					)}
 				</FormControl>
 
 				<Flex direction="column" gap={2}>
@@ -49,8 +92,16 @@ const AuthForm = () => {
 					</Flex>
 				</Flex>
 
-				<Button colorScheme="purple" borderRadius={25} mt={20}>
-					Log In
+				{errors.root && <Text color="red">{errors.root.message}</Text>}
+
+				<Button
+					type="submit"
+					colorScheme="purple"
+					borderRadius={25}
+					mt={20}
+					disabled={isSubmitting}
+				>
+					{isSubmitting ? <Spinner /> : "Log In"}
 				</Button>
 			</Flex>
 		</Box>
