f5b100976107c4f1c3db28836e5895cb8076d541
Namgel <namgeltamang14@gmail.com>
2024-08-30 05:51:36 +0545

    Conditional rendering

--- src/components/AuthModal.tsx
+++ src/components/AuthModal.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import { AuthForm, Modal } from ".";
+import { useAuthStore } from "../stores/authStore";
 
 interface Props {
 	isOpen: boolean;
@@ -6,6 +8,12 @@ interface Props {
 }
 
 const AuthModal = ({ isOpen, onClose }: Props) => {
+	const user = useAuthStore((state) => state.user);
+
+	useEffect(() => {
+		if (user) onClose();
+	}, [user]);
+
 	return (
 		<Modal isOpen={isOpen} onClose={onClose} headingText="Log In">
 			<AuthForm />
--- src/components/layouts/NavBar.tsx
+++ src/components/layouts/NavBar.tsx
@@ -1,23 +1,44 @@
-import { Box, Button, Flex, Image, useDisclosure } from "@chakra-ui/react";
+import {
+	Avatar,
+	Box,
+	Button,
+	Flex,
+	Image,
+	useDisclosure,
+} from "@chakra-ui/react";
 import { AuthModal, SearchBar } from "..";
 import logo from "../../assets/logo.png";
+import { useAuthStore } from "../../stores/authStore";
 
 const NavBar = () => {
 	const { isOpen, onOpen, onClose } = useDisclosure();
+	const user = useAuthStore((state) => state.user);
 
 	return (
 		<Box as="nav" py={2} px={5} borderBottom="1px solid #DBE4E9">
-			<Flex justify="space-between">
+			<Flex justify="space-between" align="center">
 				<Image src={logo} alt="Logo" w={150} />
 				<SearchBar />
-				<Button
-					borderRadius={20}
-					colorScheme="purple"
-					px={4}
-					onClick={onOpen}
-				>
-					Log in
-				</Button>
+
+				{user ? (
+					<Avatar
+						src=""
+						name={`${user.firstName} ${user.lastName}`}
+						size="sm"
+						color="white"
+						cursor="pointer"
+					/>
+				) : (
+					<Button
+						borderRadius={20}
+						colorScheme="purple"
+						px={4}
+						onClick={onOpen}
+						display={user ? "none" : "block"}
+					>
+						Log in
+					</Button>
+				)}
 
 				<AuthModal isOpen={isOpen} onClose={onClose} />
 			</Flex>
